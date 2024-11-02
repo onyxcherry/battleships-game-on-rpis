c781984a3e6d85dee63a8f4db9a963e5c45e1dc7
Tomasz Wiśniewski <tomasz@wisniewski.app>
2024-11-02 20:09:22 +0100

    Implement whole communication protocol for server

--- src/application/server.py
+++ src/application/server.py
@@ -1,34 +1,90 @@
 #!/usr/bin/env python
 
 import asyncio
+from typing import Optional
 from uuid import uuid4
-from application.messaging import ClientInfo
+from application.client import decode_json_message
+from application.messaging import (
+    ClientInfo,
+    ExtraInfo,
+    GameInfo,
+    GameStatus,
+    parse_client_info,
+)
 from config import get_logger
-from websockets.asyncio.server import serve
+from domain.ships import MastedShipsCounts
+from websockets.asyncio.server import serve, ServerConnection
 import websockets
 
 
-connected_clients = set()
+connected_clients: list[Optional[ServerConnection]] = [None, None]
+client_infos: list[Optional[ClientInfo]] = [None, None]
 logger = get_logger(__name__)
 
-
-async def listen(websocket):
-    connected_clients.add(websocket)
-    logger.debug(f"New client connected: {websocket.remote_address}")
-    new_client_info = ClientInfo(uniqid=uuid4(), opponent_connected=True)
-    await broadcast_message(new_client_info.stringify(), websocket)
-    try:
-        async for message in websocket:
-            logger.debug(f"Received message from {websocket.remote_address}: {message}")
-            await broadcast_message(message, websocket)
-    except websockets.ConnectionClosed as e:
-        logger.debug(f"Client {websocket.remote_address} disconnected: {e}")
-    finally:
-        connected_clients.remove(websocket)
+masted_ships_counts = MastedShipsCounts(single=4, two=3, three=2, four=1)
+board_size = 10
 
 
-async def broadcast_message(message: str, sender_socket):
+async def listen(websocket: ServerConnection):
+    if connected_clients[0] is None:
+        connected_clients[0] = websocket
+        logger.debug(f"New client connected: {websocket.remote_address}")
+        client_data = await websocket.recv()
+        client_decoded_data = decode_json_message(client_data)
+        client_info = parse_client_info(client_decoded_data)
+        client_infos[0] = client_info
+        game_info = GameInfo(
+            uniqid=uuid4(),
+            status=GameStatus.WaitingToStart,
+            opponent=None,
+            masted_ships=masted_ships_counts,
+            board_size=board_size,
+        )
+        await websocket.send(game_info.stringify())
+    elif connected_clients[1] is None:
+        connected_clients[1] = websocket
+        logger.debug(f"New client connected: {websocket.remote_address}")
+        client_data = await websocket.recv()
+        client_decoded_data = decode_json_message(client_data)
+        client_info = parse_client_info(client_decoded_data)
+        client_infos[1] = client_info
+        opponent_info = client_infos[0]
+        assert opponent_info is not None
+        can_game_start = opponent_info.ready and client_info.ready
+        game_status = (
+            GameStatus.Started if can_game_start else GameStatus.WaitingToStart
+        )
+        game_info = GameInfo(
+            uniqid=uuid4(),
+            status=game_status,
+            opponent=opponent_info,
+            masted_ships=masted_ships_counts,
+            board_size=board_size,
+        )
+        await websocket.send(game_info.stringify())
+        game_info_for_client_1 = GameInfo(
+            uniqid=uuid4(),
+            status=game_status,
+            opponent=client_info,
+            masted_ships=masted_ships_counts,
+            board_size=board_size,
+            extra=ExtraInfo(you_start_first=True),
+        )
+        await websocket.send(game_info_for_client_1.stringify())
+    else:
+        message = await websocket.recv(decode=True)
+        assert isinstance(message, str)
+        await broadcast_message(message, websocket)
+
+    # except websockets.ConnectionClosed as e:
+    #     logger.debug(f"Client {websocket.remote_address} disconnected: {e}")
+    # finally:
+    #     connected_clients.remove(websocket)
+
+
+async def broadcast_message(message: str, sender_socket: ServerConnection):
     for client in connected_clients:
+        assert client is not None
         if client != sender_socket:
             try:
                 await client.send(message)
